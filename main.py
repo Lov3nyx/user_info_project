03f4259f44859f26f63a6a93b59578ff1f57245b
Lov3nyx <cohacker12@gmail.com>
2024-09-26 08:18:18 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,28 @@
+first_name= input("please enter your name:")
+middle_initial=input("please enter your middle initial:")
+last_name=input("please enter your last name:")
+street_address=input("please enter your address:")
+city=input("please enter your city:")
+state=input("please enter your state:")
+zipcode=input("please enter your zipcode:")
+phone_number=input("please enter your phone number:")
+email_address=input("please enter your email address:")
+
+print()
+print(f"{first_name} {middle_initial} {last_name}")
+print(f"{street_address}")
+print(f"{city} {state} {zipcode}")
+print(f"{phone_number}")
+print(f"{email_address}")
+
+
+
+
+
+
+# -------------
+# print ("Hello, World!")
+# print (1+2)
+# print ("1+2")
+# print ("1"+"2")
+# print (2.4+1.65)
